a8e85355fe55ed4c84b3db64a5e34b1f9ce41e21
dell <rihabelfich@gmail.com>
2025-01-12 20:14:08 +0100

    ontology

--- sparql_project/sparql_query/forms.py
+++ sparql_project/sparql_query/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class SparqlQueryForm(forms.Form):
-    sparql_query = forms.CharField(widget=forms.Textarea, label="")
+    sparql_query = forms.CharField(widget=forms.Textarea(attrs={'rows': 5, 'cols': 80}), label="SPARQL Query")
